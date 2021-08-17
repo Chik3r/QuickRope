837b75d97797ee7b4ea2dac4a17b1f39cf633490
hamstar0 <webutek@gmail.com>
2021-08-17 05:39:52 -0700

    v0.3.0 - Added QuickRopeMod.AddRopePlacementHook(...)

--- QuickRope/QuickRopeMod.cs
+++ QuickRope/QuickRopeMod.cs
@@ -1,6 +1,34 @@
+using System.Collections.Generic;
+using Terraria;
 using Terraria.ModLoader;
 
+
 namespace QuickRope {
+	public delegate bool PlaceRopeHook( Player player, Item ropeItem, int tileX, int tileY );
+
+
+
+
 	public class QuickRopeMod : Mod {
+		public static bool AddRopePlacementHook( PlaceRopeHook hook ) {
+			return ModContent.GetInstance<QuickRopeMod>().PlaceRopeHooks.Add( hook );
+		}
+
+		////
+
+		internal static bool RunRopePlacementHooks( Player player, Item item, int tileX, int tileY ) {
+			foreach( PlaceRopeHook hook in ModContent.GetInstance<QuickRopeMod>().PlaceRopeHooks ) {
+				if( !hook.Invoke(player, item, tileX, tileY) ) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+
+
+		////////////////
+
+		internal ISet<PlaceRopeHook> PlaceRopeHooks = new HashSet<PlaceRopeHook>();
 	}
 }
