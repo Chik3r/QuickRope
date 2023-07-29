4f1cd971737bc118cfef1f3a15490e71fb880f3d
Chik3r <31808958+Chik3r@users.noreply.github.com>
2023-07-29 14:58:44 -0400

    Update to localization changes

--- RopeConfig.cs
+++ RopeConfig.cs
@@ -6,16 +6,12 @@ namespace QuickRope {
 	internal class RopeConfig : ModConfig {
 		public override ConfigScope Mode => ConfigScope.ServerSide;
 
-		[Label( "Amount of ropes to place when a rope is right clicked" )]
 		[DefaultValue( 3 )]
 		[Range( 1, 20 )]
 		[Slider]
 		[DrawTicks]
 		public int RopesToPlace = 3;
 
-		[Label( "Ticks between placement" )]
-		[Tooltip( "Amount of ticks before a rope can be placed with right click again\n" +
-				 "One second is the same as 60 ticks" )]
 		[Range( 0, 100 )]
 		[Increment( 5 )]
 		[DefaultValue( 5 )]
